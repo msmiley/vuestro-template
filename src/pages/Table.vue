59215caa6ff35b265bf0508b4f66cbce0a464041
Mike Smiley <msmiley@alumni.rice.edu>
2020-11-30 10:28:27 -0500

    add table page

---
+++ src/pages/Table.vue
@@ -0,0 +1,12 @@
+<template>
+  <vuestro-container>
+    <vuestro-card color="var(--vuestro-purple)">
+      <template #heading>Table</template>
+      <vuestro-table></vuestro-table>
+    </vuestro-card>
+  </vuestro-container>
+</template>
+
+<script>
+
+</script>
